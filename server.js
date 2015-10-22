fc8743ccbb7b8301676c10e9d3990727b024107b
cyokodog <cyokodog@gmail.com>
2015-10-22 09:56:43 +0900

    コメント文削除

--- server.js
+++ server.js
@@ -7,27 +7,3 @@ app.use(serveStatic(__dirname));
 
 app.listen(8080);
 console.log('Server running on 8080');
-
-/*
-app.use(function(req, res, next){
-  res.writeHead(200, {'Content-Type':'text/html'});
-  res.write('<h1>Hello Connect Server</h1>');
-  next();
-  res.end('<hr/>footer');
-});
-
-app.use(function(req, res, next){
-  res.writeHead(200, {'Content-Type':'text/html'});
-  res.write('<h1>Hello Connect Server</h1>');
-  next();
-  res.end('<hr/>footer');
-});
-app.use('/foo', function (req, res, next) {
-  res.write('foo');
-  next();
-});
-app.use('/bar', function (req, res, next) {
-  res.write('bar');
-  next();
-});
-*/
