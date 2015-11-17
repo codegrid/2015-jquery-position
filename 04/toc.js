2ef11220015bdbd8c0fb03f7ae302a193ea9bb6d
cyokodog <cyokodog@gmail.com>
2015-11-17 10:35:23 +0900

    リファクタ

--- 04/toc.js
+++ 04/toc.js
@@ -1,13 +1,16 @@
 $.fn.toc = function(
-  appendTo,   // 目次の挿入先
-  frame,      // 擬似フレームの指定
+  tocAppendEl,   // 目次の挿入先
+  frameEl,      // 擬似フレームの指定
   useAnimate  // animateの使用(true or false)
 ){
   // 見出し要素
   var $headings = this;
 
   // スクロールバーを持つフレーム要素の確定
-  var $frame = frame ? $(frame) : $(window);
+  var $frameEl = frameEl ? $(frameEl) : $(window);
+
+  // scrollSpyを有効にするクラス名を割り当てる
+  var $tocAppendEl = $(tocAppendEl).addClass('scrollSpy');
 
   // 目次リンクの生成
   var $links = $($headings.map(function(index, heading){
@@ -15,19 +18,19 @@ $.fn.toc = function(
   }));
 
   // 目次UIの生成
-  var $list = $('<ul/>').appendTo(appendTo);
+  var $list = $('<ul/>').appendTo(tocAppendEl);
   $links.each(function(index){
     $('<li/>').append($links.eq(index)).appendTo($list);
   });
 
   // 目次リンクのクリック
   $links.on('click', function(){
-    var index = $links.index(this)
-    scrollTo(index)
-  })
+    var index = $links.index(this);
+    scrollToHeading(index);
+  });
 
   // 指定見出し番号の位置までスクロールさせる
-  function scrollTo(index){
+  function scrollToHeading(index){
     var $heading = $headings.eq(index);
 
     // animateを使用しない
@@ -39,18 +42,29 @@ $.fn.toc = function(
 
     // animateを使用する
     else{
+      // scrollSpyを非適用
+      $tocAppendEl.removeClass('scrollSpy');
 
-      // 擬似フレーム使用時は現スクロール量分をoffset値に加算し、擬似フレームの配置位置分を減算する
-      var top = $heading.offset().top + (frame ? $frame.scrollTop() - $frame.offset().top : 0);
+      // 疑似フレーム使用時は現スクロール量分をoffset値に加算し、
+      // 疑似フレームの配置位置分を減算する
+      var diff = frameEl ?
+          $frameEl.scrollTop() - $frameEl.offset().top : 0;
+      var top = $heading.offset().top + diff;
 
-      // 擬似フレームを使用しない場合は$('html,body')でスクロールさせる
-      (frame ? $frame : $('html,body')).animate({scrollTop: top});
+      // 疑似フレームを使用しない場合は$('html,body')でスクロールさせる
+      var scrollEl = frameEl ? $frameEl : $('html,body');
+      scrollEl.animate({scrollTop: top}, function(){
+
+        // scrollSpyを適用
+        $tocAppendEl.addClass('scrollSpy');
+
+      });
     }
   }
 
   // スクロール時の処理
-  $frame.on('scroll', function(){
-    debounce(setCurrentClass)
+  $frameEl.on('scroll', function(){
+    debounce(setCurrentClass);
   });
   setCurrentClass();
 
@@ -65,7 +79,10 @@ $.fn.toc = function(
     var currentIndex;
 
     // 画面の中央位置を算出
-    var contentsTop = $frame.height() / 2 + (frame ? $frame.offset().top : $frame.scrollTop());
+    var diff = frameEl ?
+        $frameEl.offset().top : $frameEl.scrollTop();
+    var contentsTop = $frameEl.height() / 2 + diff;
+
     $headings.each(function(index){
 
       // 見出し要素が画面の中央位置を過ぎたら、その見出しをカレントとする
@@ -76,7 +93,8 @@ $.fn.toc = function(
 
     // カレントの見出しに対しクラス名を割り当てる
     if(currentIndex !== undefined){
-      $links.removeClass('current').eq(currentIndex).addClass('current');
+      $links.removeClass('current')
+            .eq(currentIndex).addClass('current');
     }
   }
 
